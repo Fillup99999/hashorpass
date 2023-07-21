5a2c62274e20f57ef563a78fa8b7a5be2c89d681
Fillup99999 <125299735+Fillup99999@users.noreply.github.com>
2023-07-20 23:00:31 -0500

    Initial commits day 1

---
+++ README.txt
@@ -0,0 +1,34 @@
+#########################################################################################################################
+###########################-HASHORPASS#ashorpassHashorPass?-#############################################################
+#########################################################################################################################
+
+Inspired by cupp.py (https://github.com/Mebus/cupp.git), this is in no way affiliated or is it plagiarism. 
+I just created a similar idea in a different format that I thought was better suited for my needs as a wordlist tool.
+I found a few flaws with the enumeration algorithm that I felt it wasted too many words and missed some important combinations that I think are more common.
+This program was created with the aid of AI as I am simply an amature programmer.
+
+
+License:
+GNU GENERAL PUBLIC LICENSE
+Version 3, 29 June 2007
+
+Preamble
+
+The GNU General Public License v3.0 is a free, copyleft license for software and other kinds of works.
+
+Terms and Conditions
+
+1. You are free to use, modify, and distribute this software under the terms of the GNU GPL v3.0 license.
+
+2. This software is provided without any warranty, expressed or implied. See the GNU GPL v3.0 license for more details.
+
+3. If you distribute this software, you must include a copy of the GNU GPL v3.0 license along with it.
+
+4. If you modify this software, you must make the modified source code available under the terms of the GNU GPL v3.0 license.
+
+5. This license applies to the entire software package, including any libraries or modules it may contain.
+
+6. This copyleft license requires the sharing of complete source code and modifications, including larger works, under the same GNU GPL v3.0 license, while preserving copyright and license notices. Contributors also grant explicit patent rights.
+
+For more information about the GNU GPL v3.0 license, please visit the Free Software Foundation's website. https://www.fsf.org/
+or open the License.txt file that comes installed in the package.
---
+++ hashorpass.py
@@ -0,0 +1,176 @@
+import argparse
+import itertools
+
+print("""Welcome to #ash or Pass?
+for help with extra options and keywords rerun as './hashorpass.py -h' basic options are prompted.""")
+
+#Options to pass arguments, this is optional and some of the options will be prompted if not selected.
+parser = argparse.ArgumentParser()
+parser.add_argument("--name", help="Enter the target's name: 'first,last,middle'")
+parser.add_argument("--dob", help="Enter the DOB 'mm.dd.yyyy.yy'")
+parser.add_argument("--bae", help="Enter spouse/partner's name = 'first,mm.dd.yyyy.yy'")
+parser.add_argument("--kid", help="Enter favorite child's name and dob = 'stephanie,mm.dd.yyyy.yy'")
+parser.add_argument("--kid2", help="Enter child's name = 'franklin,mm.dd.yyyy.yy'")
+parser.add_argument("--kid3", help="Enter child's name = 'richierich,mm.dd.yyyy.yy'")
+parser.add_argument("--pet", help="Enter up to 5 pet or friend names = 'spike,spot,keke,glubglub'")
+parser.add_argument("--sport", help="Enter favorite sport teams and chants (up to 5 inputs) 'blueeagles,tokyostars,benchwarmers'")
+parser.add_argument("--yrs", help="Enter up to 5 important years '1945,1776,0033'")
+parser.add_argument("--key", help="Enter up to 10 keywords or hobbies 'photography,butts,hacking'")
+parser.add_argument("-a", action="store_true", help="option a")
+parser.add_argument("-b", action="store_true", help="option b")
+parser.add_argument("-c", action="store_true", help="option c")
+parser.add_argument("-d", action="store_true", help="option d")
+args = parser.parse_args()
+
+#Input options for if no args were passed.
+
+if args.name:
+      target_name_split = args.name.split(",")
+      target_first_name = target_name_split[0]
+      target_last_name = target_name_split[1]
+      target_middle_name = target_name_split[2]
+      target_middle_i = target_middle_name[0]
+
+else:
+      target_first_name = input("Enter the target's first name: ")
+      target_last_name = input("Enter the target's last name: ")
+      target_middle_name = input("Enter the target's middle name or initial: ")
+      target_middle_i = target_middle_name[0]
+
+if args.dob:
+      dob_split = args.name.split(".")
+      birthyyyy = dob_split[2]
+      birthyy = dob_split[3]
+      birthmm = dob_split[0]
+      birthdd = dob_split[1]
+else:
+      date_of_birth = input("Enter the Date of Birth: mm.dd.yyyy.yy: ")
+      splitbirth = date_of_birth.split(".")
+      birthyyyy = splitbirth[2]
+      birthyy = splitbirth[3]
+      birthmm = splitbirth[0]
+      birthdd = splitbirth[1]
+
+
+#Remaining options:
+
+#Spouse/Partner
+baelist = args.bae.split(",")
+baename = baelist[0]
+baedob = baelist[1]
+baedoblist = baedob.split(".")
+baemm = baedoblist[0]
+baedd = baedoblist[1]
+baeyyyy = baedoblist[2]
+baeyy = baedoblist[3]
+
+#Favorite Kid
+kidlist = args.kid.split(",")
+kidname = kidlist[0]
+kiddob = kidlist[1]
+kiddoblist = kiddob.split(".")
+kidmm = kiddoblist[0]
+kiddd = kiddoblist[1]
+kidyyyy = kiddoblist[2]
+kidyy = kiddoblist[3]
+
+#Kid 2
+kid2list = args.kid2.split(",")
+kid2name = kid2list[0]
+kid2dob = kid2list[1]
+kid2doblist = kid2dob.split(".")
+kid2mm = kid2doblist[0]
+kid2dd = kid2doblist[1]
+kid2yyyy = kid2doblist[2]
+kid2yy = kid2doblist[3]
+
+#Kid 3
+kid3list = args.kid3.split(",")
+kid3name = kid3list[0]
+kid3dob = kid3list[1]
+kid3doblist = kid3dob.split(".")
+kid3mm = kid3doblist[0]
+kid3dd = kid3doblist[1]
+kid3yyyy = kid3doblist[2]
+kid3yy = kid3doblist[3]
+
+#Pets/Friends
+petlist = args.pet.split(",")
+pet1 = petlist[0]
+pet2 = petlist[1]
+pet3 = petlist[2]
+pet4 = petlist[3]
+pet5 = petlist[4]
+
+#Sports
+sportlist = args.sport.split(",")
+sport1 = sportlist[0]
+sport2 = sportlist[1]
+sport3 = sportlist[2]
+sport4 = sportlist[3]
+sport5 = sportlist[4]
+
+#Important Years
+yrslist = args.yrs.split(",")
+yr1 = yrslist[0]
+#last two year digits
+shyr1 = yr1[2:]
+yr2 = yrslist[1]
+shyr2 = yr2[2:]
+yr3 = yrslist[2]
+shyr3 = yr3[2:]
+yr4 = yrslist[3]
+shyr4 = yr4[2:]
+yr5 = yrslist[4]
+shyr5 = yr5[2:]
+
+#Keywords
+keylist = args.key.split(",")
+key1 = keylist[0]
+key2 = keylist[1]
+key3 = keylist[2]
+key4 = keylist[3]
+key5 = keylist[4]
+key6 = keylist[5]
+key7 = keylist[6]
+key8 = keylist[7]
+key9 = keylist[8]
+key10 = keylist[9]
+
+
+print(target_first_name,target_last_name,target_middle_name,target_middle_i,date_of_birth, splitbirth, birthyyyy, birthyy, birthmm, birthdd)
+print(baelist, baename,baedob,baedoblist,baemm,baedd,baeyyyy,baeyy)
+
+
+
+#SAMPLEEEE
+#Password Generator:
+import random
+import string
+
+# Custom password generation
+def generate_custom_password():
+    # Implement your custom password generation logic here
+    # This can include combining different elements, patterns, or using specific rules
+    # Here's a simple example:
+    password = target_first_name.lower() + target_last_name.lower() + random.choice(string.digits)
+    return password
+
+# Brute force password generation using a word list
+def generate_brute_force_password():
+    word_list = ["apple", "banana", "cherry", "orange", "grape"]
+    password = random.choice(word_list) + random.choice(word_list) + random.choice(string.ascii_uppercase) + random.choice(string.digits) + random.choice(string.punctuation)
+    return password
+
+# Generate passwords
+passwords = []
+for _ in range(5):  # Generate 5 passwords
+    if random.random() < 0.5:  # 50% chance of using custom password generation
+        password = generate_custom_password()
+    else:  # 50% chance of using brute force password generation
+        password = generate_brute_force_password()
+    passwords.append(password)
+
+# Print the generated passwords
+for password in passwords:
+    print(password)
